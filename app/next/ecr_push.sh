e5f89c21fedb0bc3567eaea764e4c6bae709fce0
Yutaro Hayashi <yutaro50@gmail.com>
2022-06-16 11:48:25 +0900

    ディレクトリ構成変更

---
+++ app/next/ecr_push.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+GIT_COMMIT_ID=$(git log --format="%H" -n 1)
+aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 412335340103.dkr.ecr.us-east-1.amazonaws.com
+docker build -t practice_terraform_next:"${GIT_COMMIT_ID}" -f Dockerfile.prod .
+docker tag practice_terraform_next:"${GIT_COMMIT_ID}" 412335340103.dkr.ecr.us-east-1.amazonaws.com/practice_terraform_next:"${GIT_COMMIT_ID}"
+docker push 412335340103.dkr.ecr.us-east-1.amazonaws.com/practice_terraform_next:"${GIT_COMMIT_ID}"
