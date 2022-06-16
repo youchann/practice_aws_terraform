0529c7ea5a4afbf498a93936c1834195e23a2c2d
Yutaro Hayashi <yutaro50@gmail.com>
2022-06-16 10:06:29 +0900

    laravelのいろいろ追加

---
+++ laravel/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('test', static function () {
+    return array('sample test');
+});
+
+Route::post('test', static function (Request $request) {
+    $request->validate(['text' => 'required|string']);
+    return \App\Models\Sample::create(['text' => $request->input('text')]);
+});
