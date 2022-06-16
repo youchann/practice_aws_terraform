ac7f46e9cde5bf2281f8cca5a6e64b276d925bcd
Yutaro Hayashi <yutaro50@gmail.com>
2022-06-16 11:54:04 +0900

    setup terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+	required_providers {
+	  aws = {
+		source = "hashicorp/aws"
+		version = ">= 4.1.0"
+	  }
+	}
+}
+
+locals {
+  app_name = "terraform_practice"
+}
+
+provider "aws" {
+	region = "ap-northeast-1"
+	default_tags {
+		tags = {
+			application = local.app_name
+		}
+	}
+}
