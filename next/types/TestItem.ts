81b0abca8ee43ac90110605bdf3c7990c40e0f14
Yutaro Hayashi <yutaro50@gmail.com>
2022-06-16 10:02:57 +0900

    add new project experience

---
+++ next/types/TestItem.ts
@@ -0,0 +1,8 @@
+export type  TestItem = Pick<TestItemResponse, "text" | "updated_at" | "created_at" | 'id'>
+
+export interface TestItemResponse {
+    id: number,
+    text: string,
+    updated_at: any,
+    created_at: any
+}
